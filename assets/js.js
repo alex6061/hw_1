3b801f337bec546a4f3ee60f46deec6d1c1a27c6
alex6061 <39526012+alex6061@users.noreply.github.com>
2023-03-29 21:38:52 +0300

    commit

---
+++ assets/js.js
@@ -0,0 +1,9 @@
+ // Функция изменения цвета фона веб-страницы
+ function changeBodyBg(color){
+  document.body.style.background = color;
+}
+
+// Функция изменения цвета фона заголовка
+function changeHeadingBg(color){
+  document.getElementById("heading").style.background = color;
+}
